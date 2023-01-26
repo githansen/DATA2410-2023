a132aebc49a8b360672f60927376a7fb4d4487b7
Johan Hustoft Hansen <s349880@oslomet.no>
2023-01-26 12:38:03 +0100

    Complete

--- Client.py
+++
@@ -1,8 +0,0 @@
-import socket 
-import sys
-host = socket.gethostbyname(socket.gethostname())  # Host IP
-print(host)
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((host, 80))
-msg = sys.argv[1]
-socket.send(msg.encode())
